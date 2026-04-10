093b33072b9caf348570090b9a703118ed6d50d9
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-10 20:35:09 +0600

    started learing nextjs, got a concept of basic routing

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+
+const AboutPage = () => {
+    return (
+        <div className="text-center text-5xl font-black mt-50">
+            <h2>This is about page</h2>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/contact/developer/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeveloperPage = () => {
+    return (
+        <div>
+            <h2>This is developer Page</h2>
+        </div>
+    );
+};
+
+export default DeveloperPage;
