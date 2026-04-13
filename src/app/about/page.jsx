a559535dda0849a58b9ad30a7a2a4a46e71616c2
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-13 13:23:48 +0600

    used image rendering in nextjs

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 
 const AboutPage = () => {
     return (
-        <div className="text-center text-5xl font-black mt-50">
+        <div className="text-center text-5xl font-black mt-20 flex flex-col justify-center items-center">
             <h2>This is about page</h2>
+            <Image src='/miku-loading.gif' alt="" width={500} height={500}></Image>
         </div>
     );
 };
